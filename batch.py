f84636831d32f8f5972e6446a671bb9170869d8a
frugeles <f.rugeles@gmail.com>
2016-12-21 18:23:20 +0100

    Unify batch processes

--- batch.py
+++ batch.py
@@ -12,45 +12,58 @@ def batch(db):
     # in_file = 'data/adresses_vAMABIS_v28092016_out_v2.csv'
     in_file = 'data/ADRESSES_PART_GEO_AMABIS_v01072016_out.csv'
     out_file = 'data/ADRESSES_PART_GEO_AMABIS_v01072016_geocoding_julien.csv'
-    with open(in_file, 'r') as addresses, \
-            open(out_file, 'w') as out:
-        i = 0
-        for line in addresses:
+    total_error = 0
+    with open(in_file, 'r',encoding='UTF-8') as addresses, \
+            open(out_file, 'w',encoding='UTF-8') as out:
+        
+        for i, line in enumerate(addresses):
             line = line[:-1].replace('"', '')
             if i == 0:
-                out.write(line+';locality;number;street;code_post;city;code_insee;country;distance;lon;lat;time\n')
-                i = 1
+                out.write(line+';locality;number;street;code_post;city;code_insee;country;quality;distance;error;lon;lat;time\n')
                 continue
             values = line.split(';')
             try:
                 code_post = values[5].zfill(5)
                 city = values[6]
-                query = values[3]
-                address = main.search_by_zip_and_city(db, code_post, city,
-                                                      query)
-                d = None
-                if address['lon'] != 'None' and address['lat'] != 'None':
-                    lon1 = float(values[32])
-                    lat1 = float(values[33])
-                    lon2 = float(address['lon'])
-                    lat2 = float(address['lat'])
-                    d = haversine(lon1, lat1, lon2, lat2)
-                values += [
-                    address['locality'],
-                    address['number'],
-                    address['street'],
-                    address['code_post'],
-                    address['city'],
-                    address['code_insee'],
-                    address['country'],
-                    str(d),
-                    address['lon'],
-                    address['lat'],
-                    address['time'],
-                    ]
-            except:
-                pass
+                query = values[3] or values[2] or values[1]
+                address = search.search_by_zip_and_city(db, code_post, city,
+                                                      query).to_address()
+                d = 100000
+                if values[32] and values[33]:
+                    if address['lon'] and address['lat']:
+                        lon1 = float(values[32])
+                        lat1 = float(values[33])
+                        lon2 = float(address['lon'])
+                        lat2 = float(address['lat'])
+                        d = haversine(lon1, lat1, lon2, lat2)
+                    else:
+                        d = 0
+                    if d == 100000:
+                        print(i)
+                    error = d
+                    values += [
+                        address['locality'],
+                        address['number'],
+                        address['street'],
+                        address['code_post'],
+                        address['city'],
+                        address['code_insee'],
+                        address['country'],
+                        address['quality'].value,
+                        str(round(d, 2)),
+                        str(round(error, 6)),
+                        address['lon'],
+                        address['lat'],
+                        address['time'],
+                        ]
+                    total_error += error
+            except Exception as e:
+                print(e)
+                traceback.print_exc()
             out.write(";".join(map(str, values)) + '\n')
+            if i % 1000 == 0:
+                print(i, 'th ERROR: ', round(total_error/(i-1), 2))
+        print('FINAL ERROR: ', round(total_error/(i-1), 2))
 
 
 def batch2(db):
@@ -58,8 +71,8 @@ def batch2(db):
     in_file = 'data/adresses_vAMABIS_v28092016_out_v2.csv'
     out_file = 'data/adresses_vAMABIS_v28092016_out_v2_geocoding_julien.csv'
     total_error = 0
-    with open(in_file, 'r') as addresses, \
-            open(out_file, 'w') as out:
+    with open(in_file, 'r',encoding='UTF-8') as addresses, \
+            open(out_file, 'w',encoding='UTF-8') as out:
         for i, line in enumerate(addresses):
             line = line[:-1].replace('"', '')
             if i == 0:
@@ -131,7 +144,7 @@ def save_big_error():
 
 if __name__ == '__main__':
     db = main.AddressDatabase()
-    # batch()
+    batch(db)
     batch2(db)
-    calculate_metrics()
-    save_big_error()
+    #calculate_metrics()
+    #save_big_error()
