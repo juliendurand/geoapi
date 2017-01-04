15c219d0ea06d5bf58d2ec2b3ae06560588aff47
Julien Durand <julien.durand@axa.fr>
2017-01-04 21:09:11 +0100

    refactors batch.py

--- batch.py
+++ batch.py
@@ -8,20 +8,44 @@ import search
 from utils import haversine
 
 
-def batch(db):
-    # in_file = 'data/adresses_vAMABIS_v28092016_out_v2.csv'
-    in_file = 'data/ADRESSES_PART_GEO_AMABIS_v01072016_out.csv'
-    out_file = 'data/ADRESSES_PART_GEO_AMABIS_v01072016_geocoding_julien.csv'
+def detect_separator(line):
+    separator = None
+    max_nb_tokens = 0
+    for c in ',;#|:':
+        nb_tokens = len(line.split(c))
+        if nb_tokens > max_nb_tokens:
+            separator = c
+            max_nb_tokens = nb_tokens
+    return separator
+
+
+def batch(db, in_file, out_file):
     total_error = 0
     with open(in_file, 'r', encoding='UTF-8') as addresses, \
             open(out_file, 'w', encoding='UTF-8') as out:
-
+        header = addresses.readline()[:-1].replace('"', '')
+        separator = detect_separator(header)
+        headers = header.split(separator)
+        lon_index = headers.index('LON')
+        lat_index = headers.index('LAT')
+        new_headers = [
+            'locality',
+            'number',
+            'street',
+            'code_post',
+            'city',
+            'code_insee',
+            'country',
+            'quality',
+            ' distance',
+            'error',
+            'lon',
+            'lat',
+            'time',
+        ]
+        out.write(separator.join(headers + new_headers) + '\n')
         for i, line in enumerate(addresses):
             line = line[:-1].replace('"', '')
-            if i == 0:
-                out.write(line + ';locality;number;street;code_post;city;\
-code_insee;country;quality;distance;error;lon;lat;time\n')
-                continue
             values = line.split(';')
             try:
                 code_post = values[5].zfill(5)
@@ -30,10 +54,10 @@ code_insee;country;quality;distance;error;lon;lat;time\n')
                 address = search.search_by_zip_and_city(db, code_post, city,
                                                         query).to_address()
                 d = 100000
-                if values[32] and values[33]:
+                if values[lon_index] and values[lat_index]:
                     if address['lon'] and address['lat']:
-                        lon1 = float(values[32])
-                        lat1 = float(values[33])
+                        lon1 = float(values[lon_index])
+                        lat1 = float(values[lat_index])
                         lon2 = float(address['lon'])
                         lat2 = float(address['lat'])
                         d = haversine(lon1, lat1, lon2, lat2)
@@ -63,66 +87,8 @@ code_insee;country;quality;distance;error;lon;lat;time\n')
                 traceback.print_exc()
             out.write(";".join(map(str, values)) + '\n')
             if i % 1000 == 0:
-                print(i, 'th ERROR: ', round(total_error / (i - 1), 2))
-        print('FINAL ERROR: ', round(total_error / (i - 1), 2))
-
-
-def batch2(db):
-    # in_file = 'data/adresses_vAMABIS_v28092016_out_v2.csv'
-    in_file = 'data/adresses_vAMABIS_v28092016_out_v2.csv'
-    out_file = 'data/adresses_vAMABIS_v28092016_out_v2_geocoding_julien.csv'
-    total_error = 0
-    with open(in_file, 'r', encoding='UTF-8') as addresses, \
-            open(out_file, 'w', encoding='UTF-8') as out:
-        for i, line in enumerate(addresses):
-            line = line[:-1].replace('"', '')
-            if i == 0:
-                out.write(line + ';locality;number;street;code_post;city;\
-code_insee;country;quality;distance;error;lon;lat;time\n')
-                continue
-            values = line.split(';')
-            try:
-                code_post = values[5].zfill(5)
-                city = values[6]
-                query = values[3] or values[2] or values[1]
-                address = search.search_by_zip_and_city(db, code_post, city,
-                                                        query).to_address()
-                d = 100000
-                if values[31] and values[32]:
-                    if address['lon'] and address['lat']:
-                        lon1 = float(values[31])
-                        lat1 = float(values[32])
-                        lon2 = float(address['lon'])
-                        lat2 = float(address['lat'])
-                        d = haversine(lon1, lat1, lon2, lat2)
-                else:
-                    d = 0
-                if d >= 100000:
-                    print(i, d)
-                error = d
-                values += [
-                    address['locality'],
-                    address['number'],
-                    address['street'],
-                    address['code_post'],
-                    address['city'],
-                    address['code_insee'],
-                    address['country'],
-                    address['quality'].value,
-                    str(round(d, 2)),
-                    str(round(error, 6)),
-                    address['lon'],
-                    address['lat'],
-                    address['time'],
-                ]
-                total_error += error
-            except Exception as e:
-                print(e)
-                traceback.print_exc()
-            out.write(";".join(map(str, values)) + '\n')
-            if i % 1000 == 0:
-                print(i, 'th ERROR: ', round(total_error / (i - 1), 2))
-        print('FINAL ERROR: ', round(total_error / (i - 1), 2))
+                print(i, 'th ERROR: ', round(total_error / (i + 1), 2))
+        print('FINAL ERROR: ', round(total_error / (i + 1), 2))
 
 
 def calculate_metrics():
@@ -150,7 +116,9 @@ def save_big_error():
 
 if __name__ == '__main__':
     db = main.AddressDatabase()
-    batch(db)
-    batch2(db)
+    batch(db, 'data/ADRESSES_PART_GEO_AMABIS_v01072016_out.csv',
+          'data/ADRESSES_PART_GEO_AMABIS_v01072016_geocoding_julien.csv')
+    batch(db, 'data/adresses_vAMABIS_v28092016_out_v2.csv',
+          'data/adresses_vAMABIS_v28092016_out_v2_geocoding_julien.csv')
     calculate_metrics()
     save_big_error()
