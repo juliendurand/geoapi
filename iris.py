fe3e65c7ba8ef095106022fa6c28c2f9ae046780
Julien Durand <julien.durand@axa.fr>
2017-01-23 19:38:32 +0100

    apache license for iris.py

--- iris.py
+++ iris.py
@@ -1,7 +1,21 @@
+"""
+Copyright (C) 2016 Julien Durand
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import geopandas as gpd
 from shapely.geometry import Point
-
-from utils import conv_wsg84_to_lambert93
 
 print('Loading IRIS Shapefile')
 # iris = gpd.read_file('./data/CONTOURS-IRIS_2-1__SHP_LAMB93_FXX_2016-11-10/CONTOURS-IRIS/1_DONNEES_LIVRAISON_2015/CONTOURS-IRIS_2-1_SHP_LAMB93_FE-2015/CONTOURS-IRIS.shp')
