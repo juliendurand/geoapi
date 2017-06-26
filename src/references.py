8ef2568a59744264d7f97e2ab6800c65bed7949e
Paulo <paulo-emilio.de-vilhena@axa.fr>
2017-06-26 11:01:57 +0200

    Adds module with references to all the useful directories

---
+++ src/references.py
@@ -0,0 +1,21 @@
+DATA_DIR = 'data'
+BAN_SUBDIR = 'ban'
+INTERMEDIATE_DIR = 'database_csv'
+INDEX_DIR = 'database_dat'
+
+CITY_DB = 'cities.dat'
+STREET_DB = 'streets.dat'
+LOCALITIES_DB = 'localities.dat'
+NUMBER_DB = 'number.dat'
+
+CITY_CSV = 'cities.csv'
+STREET_CSV = 'streets.csv'
+LOCALITY_CSV = 'locality.csv'
+NUMBER_CSV = 'numbers.csv'
+REPETITION_JSON = 'repetitions.json'
+
+CITIES_POST_INDEX = 'cities_post_index.dat'
+STREETS_INSEE_INDEX = 'streets_insee_index.dat'
+LOCALITIES_INSEE_INDEX = 'localities_insee_index.dat'
+NUMBERS_LOCALITY_INDEX = 'numbers_locality_index.dat'
+NUMBERS_GEOHASH_INDEX = 'numbers_geohash_index.dat'
